e8bba5e8ff1bdd7a3dd50689af69c7fc0b4a19d4
Abraham Guerrero J <abraham.gj44@gmail.com>
2024-10-28 15:39:20 -0500

    final

--- processing.py
+++ processing.py
@@ -2,8 +2,8 @@ from sklearn.cluster import KMeans
 import polars as pl
 import plotly.express as px
 
-path2 = r"C:\Users\abrah\OneDrive\Desktop\final-dataset_rangos_V4.xlsx"
-df = pl.read_excel(path2, sheet_name="Sheet 1")
+path2 = r"C:\Users\abrah\OneDrive\Desktop\casoCAS\archivo\data\final-dataset_V.5.5.xlsx"
+df = pl.read_excel(path2, sheet_name="Sheet1")
 
 
 def cluster_dimension(
@@ -42,21 +42,66 @@ def cluster_dimension(
     return cluster_df
 
 
-f = df.group_by("Ciudad").agg(pl.col("Suma Asegurada").sum(), pl.col("Prima").sum())
-f
-h = cluster_dimension(f, "Ciudad", ["Suma Asegurada", "Prima"], n_clusters=4)
+f = (
+    df.group_by("Ciudad")
+    .agg(
+        pl.col("Exposicion").sum().alias("Exposicion"),
+        pl.col("Suma Asegurada").sum(),
+        pl.col("Prima").sum().alias("Prima Ganada"),
+        pl.col("Evento ID").drop_nulls().n_unique().alias("Número Eventos"),
+        pl.col("Numero Siniestros").sum(),
+        pl.col("Duración de la inundación (días)").mean().alias("Duración Promedio"),
+        pl.col("Severidad de la inundación (escala 1-5)")
+        .mean()
+        .alias("Magnitud Inundaciones Promedio"),
+        pl.col("Precipitación (mm)").mean().alias("Precipitación Promedio"),
+        pl.col("Incremento del Nivel del Río (m)").mean().alias("Incremento Promedio"),
+        pl.col("Monto de siniestro").sum().alias("Incurrido"),
+    )
+    .with_columns(
+        (pl.col("Incurrido") / pl.col("Prima Ganada")).alias(
+            "Indice de Siniestralidad"
+        ),
+        (pl.col("Prima Ganada") / pl.col("Suma Asegurada") * 1000).alias(
+            "Tasa por Mil"
+        ),
+        (pl.col("Incurrido") / pl.col("Suma Asegurada")).alias("TPR"),
+        (pl.col("Numero Siniestros") / pl.col("Exposicion")).alias("Frecuencia"),
+        (pl.col("Incurrido") / pl.col("Numero Siniestros")).alias("Severidad"),
+    )
+    .with_columns((pl.col("Frecuencia") * pl.col("Severidad")).alias("Prima Pura"))
+    .with_columns((pl.col("Prima Pura") / pl.col("Prima Ganada")).alias("proportion"))
+    .with_columns(
+        (pl.col("Suma Asegurada") / pl.col("Exposicion")).alias(
+            "Suma Asegurada Promedio"
+        )
+    )
+    .with_columns(
+        (pl.col("Prima Pura") / pl.col("Suma Asegurada") * 1000).alias(
+            "Tasa por mil inundación"
+        )
+    )
+    .sort(by="Indice de Siniestralidad", descending=False)
+)
 
-px.scatter(h, y="Prima", x="Ciudad", color="CiudadClustered", size="Suma Asegurada")
+f
+h = cluster_dimension(f, "Ciudad", ["Severidad"], n_clusters=4)
+
+px.scatter(
+    h, y="Severidad", x="Ciudad", color="CiudadClustered", size="Numero Siniestros"
+)
 
 graph = px.scatter(
     h,
-    y="Prima",
+    y="Severidad",
     x="Ciudad",
-    color="CiudadClustered",
-    size="Suma Asegurada",
+    color="SeveridadClustered",
+    size="Numero Siniestros",
     marginal_y="box",
     template="plotly_white",
 )
 graph
 
-graph.write_html(r"C:\Users\abrah\OneDrive\Desktop\dark.html")
+graph.write_html(
+    r"C:\Users\abrah\OneDrive\Desktop\casoCAS\docs\images\clustersCiudad.html"
+)
